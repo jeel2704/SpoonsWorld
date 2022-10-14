4764241d43cea15449ed574ddd5230b78d55c76b
jeel2704 <jp196@myscc.ca>
2022-10-14 04:18:56 -0400

    add model

---
+++ Models/Spoon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpoonsWorld.Models
+{
+    public class Spoon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public string Brand { get; set; }
+        public decimal weight { get; set; }
+        public int NumberOfPieces { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -1,5 +1,11 @@
 ﻿4:05am started assignment my topic is spoons
-4:06 give the name to project spoonsworld
+4:06am give the name to project spoonsworld
  create mvc webapp
  run the webapp it work successfully
  upload assignment to github
+
+ 4:16
+ add model name spoon.cs file to webapp
+and give properties id, name , material, brand, weight, number of pieces and price.
+run the app it work successfully
+upload code to github
